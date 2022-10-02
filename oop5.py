29125eb008a555ebc0f60211492f57414d6acaa1
AdityRiya <66531291+AdityRiya@users.noreply.github.com>
2022-10-02 20:59:42 +0600

    update oop5.py

--- oop5.py
+++ oop5.py
@@ -9,5 +9,8 @@ class Circle():
     def set_r(self,new_r):
         self.radius = new_r
 
-myc= Circle()
-print(myc.radius)       
+myc= Circle(5)
+print(myc.radius)      
+print(myc.area())   
+myc.set_r(100)
+print(myc.area())  
