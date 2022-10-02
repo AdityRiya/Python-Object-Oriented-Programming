94fd1ae8df8ae3aeb29b797397a4b83138c1252b
AdityRiya <66531291+AdityRiya@users.noreply.github.com>
2022-10-02 20:51:55 +0600

    update oop5.py

--- oop5.py
+++ oop5.py
@@ -1,9 +1,13 @@
 class Circle():
     pi = 3.1415
-    def __init__(self,radius = 1):
+    def __init__(self,radius=1):
         self.radius = radius
-    def area():
-        return self.radius * self.radius * Circle.pi
         
-    def set_r():
-        
+    def area(self):
+        return self.radius*self.radius * Circle.pi
+    
+    def set_r(self,new_r):
+        self.radius = new_r
+
+myc= Circle()
+print(myc.radius)       
