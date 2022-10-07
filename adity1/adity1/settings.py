f9c43534905a8d2d54809ed5632ebc0beba76527
AdityRiya <66531291+AdityRiya@users.noreply.github.com>
2022-10-07 10:38:43 +0600

    new line shown

--- adity1/adity1/settings.py
+++ adity1/adity1/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "adity1_app"
+    "adity1_app",
+    "adity1_app2"
 ]
 
 MIDDLEWARE = [
--- adity1/adity1_app2/views.py
+++ adity1/adity1_app2/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
-def show(text):
-    HttpResponse("<em>The Text in embedded</em>")
+def index(text):
+    return HttpResponse("<em>The Text in embedded</em>")
