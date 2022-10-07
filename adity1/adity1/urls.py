01870c027663a7ce7c1b4d109ce8bde340b9e3e5
AdityRiya <66531291+AdityRiya@users.noreply.github.com>
2022-10-07 10:10:51 +0600

    update

--- adity1/adity1/urls.py
+++ adity1/adity1/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from adity1_app import views
 urlpatterns = [
+    path("index/",views.index),
     path("admin/", admin.site.urls),
 ]
